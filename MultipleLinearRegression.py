109cad05699854396e1911f2dcb6dd4c3bddf1dd
kyunji <kyunji@ajou.ac.kr>
2022-06-05 23:13:02 +0900

    FIX : 모델 코드 수정, 데이터 수정

--- MultipleLinearRegression.py
+++ MultipleLinearRegression.py
@@ -1,23 +1,22 @@
 from sklearn.linear_model import LinearRegression
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("Umandong100.csv")
+train=pd.read_csv('TrainData.csv')
+test=pd.read_csv('TestData.csv')
+X_train=train[['X1','X2','X3']]
+y_train=train['Y']
+X_test=test[['X1','X2','X3']]
+y_test=test['Y']
 
-X = data[['x1','x2','x3']]
-y = data[['y']]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=10000)
-
-mlr = LinearRegression()
-mlr.fit(X_train, y_train)
-
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred=model.predict(X_test)
 #R square
-print("정확도",mlr.score(X_test, y_test))
-print(mlr.predict([[6,18,3]]))
+print("정확도",model.score(X_test, y_test))
 
-y_predict = mlr.predict(X_test)
+y_predict = model.predict(X_test)
 plt.scatter(y_test, y_predict, alpha=0.4)
 plt.xlabel("Actual Total Time")
 plt.ylabel("Predicted Total Time")
